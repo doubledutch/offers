94925e23741bb15adf0783eecea1c701da71f999
Adam Liechty <adam@doubledutch.me>
2019-03-01 16:53:59 -0800

    fix i18n paths

--- mobile/src/i18n/index.js
+++ mobile/src/i18n/index.js
@@ -1,12 +1,12 @@
-import enUS from './en-us/offers.admin.json'
+import enUS from './en-us/offers.mobile.json'
 
-import arINT from './ar-int/offers.admin.json'
-import deDE from './de-de/offers.admin.json'
-import esES from './es-es/offers.admin.json'
-import frFR from './fr-fr/offers.admin.json'
-import itIT from './it-it/offers.admin.json'
-import jaJP from './ja-jp/offers.admin.json'
-import nlNL from './nl-nl/offers.admin.json'
+import arINT from './ar-int/offers.mobile.json'
+import deDE from './de-de/offers.mobile.json'
+import esES from './es-es/offers.mobile.json'
+import frFR from './fr-fr/offers.mobile.json'
+import itIT from './it-it/offers.mobile.json'
+import jaJP from './ja-jp/offers.mobile.json'
+import nlNL from './nl-nl/offers.mobile.json'
 
 export default {
   '': enUS,
